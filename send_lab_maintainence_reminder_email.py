ca4511eb69a989f9697f85782ad3e087f06309c4
LFL-Lab <lflab.bot@gmail.com>
2023-07-10 13:49:14 -0700

    calendar event creation feature added

--- send_lab_maintainence_reminder_email.py
+++ send_lab_maintainence_reminder_email.py
@@ -11,15 +11,28 @@ __author__ =  "Sadman Ahmed Shanto"
 __date__ = "07/18/2022"
 __email__ = "shanto@usc.edu"
 
-import datetime
 from HelperFunctions import *
+from datetime import datetime, timedelta
 
 if __name__ == "__main__":
     # weekday choice (day in advance)
-    date_maintenance = str(datetime.date.today() + datetime.timedelta(days=1))
+    #date_maintenance = str(datetime.date.today() + datetime.timedelta(days=1))
+    
+    # Get today's date
+    date_maintenance = datetime.today().date()
+
+    # Get the date five days from today
+    end_date = date_maintenance + timedelta(days=5)
+
+    # Convert dates to strings
+    start_date_str = str(date_maintenance)
+    end_date_str = str(end_date)
 
     # liquid nitrogen appt time
     ln2time = "15:00"
+    
+    # Define the event details
+    location = "LFL Lab"
 
     # content of email
     ln2_instruction = "Please schedule a Liquid Nitrogen Fill Up with Jivin (jseward@usc.edu) and refill our tank"
@@ -30,12 +43,16 @@ if __name__ == "__main__":
     """
     Logic:
     """
+
     # read emission file to see whose turn this week (i.e. at execution)
     recipient_name, recipient_email = extract_lab_maintainer()
 
     # create email for lab maintainer
     email_content = create_email_content(recipient_name, date_maintenance, instructions, reminders)
     subjectLine = "LFL Lab Maintenance Reminder ({})".format(date_maintenance)
+
+    # Create the event
+    event_link = create_event_with_dates(subjectLine, location, email_content, start_date_str, end_date_str)
 
     # send email to lab maintainer
     send_email(recipient_email, subjectLine, email_content)
